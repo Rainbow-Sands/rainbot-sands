9a0d607c0c8296eede2b99ea773e5527179ed634
Daniel Power <me@danielpower.ca>
2023-10-04 01:40:19 -0230

    Fix background image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 		margin: 0;
 		padding: 0;
 		font-family: sans-serif;
-		background: url('dunes.jpg') no-repeat center fixed;
+		background: url('/dunes.jpg') no-repeat center fixed;
 		background-size: cover;
 	}
 </style>
